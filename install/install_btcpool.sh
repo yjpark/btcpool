bef98138b60610352f12ace536207a5f130a2c9c
YJ Park <yjpark@gmail.com>
2017-09-12 13:42:11 +0800

    tweak install folders

--- install/install_btcpool.sh
+++ install/install_btcpool.sh
@@ -11,11 +11,12 @@
 
 CPUS=`lscpu | grep '^CPU(s):' | awk '{print $2}'`
 
-apt-get update
-apt-get install -y build-essential autotools-dev libtool autoconf automake pkg-config cmake
-apt-get install -y openssl libssl-dev libcurl4-openssl-dev libconfig++-dev libboost-all-dev libmysqlclient-dev libgmp-dev libzookeeper-mt-dev
+#apt-get update
+#apt-get install -y build-essential autotools-dev libtool autoconf automake pkg-config cmake wget
+#apt-get install -y openssl libssl-dev libwget4-openssl-dev libconfig++-dev libboost-all-dev libmysqlclient-dev libgmp-dev libzookeeper-mt-dev
+
 # zmq-v4.1.5
-mkdir -p /root/source && cd /root/source
+mkdir -p /btcpool/source && cd /btcpool/source
 wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.5/zeromq-4.1.5.tar.gz
 tar zxvf zeromq-4.1.5.tar.gz
 cd zeromq-4.1.5
@@ -23,7 +24,7 @@ cd zeromq-4.1.5
 make check && make install && ldconfig
 
 # glog-v0.3.4
-mkdir -p /root/source && cd /root/source
+mkdir -p /btcpool/source && cd /btcpool/source
 wget https://github.com/google/glog/archive/v0.3.4.tar.gz
 tar zxvf v0.3.4.tar.gz
 cd glog-0.3.4
@@ -31,14 +32,14 @@ cd glog-0.3.4
 
 # librdkafka-v0.9.1
 apt-get install -y zlib1g zlib1g-dev
-mkdir -p /root/source && cd /root/source
+mkdir -p /btcpool/source && cd /btcpool/source
 wget https://github.com/edenhill/librdkafka/archive/0.9.1.tar.gz
 tar zxvf 0.9.1.tar.gz
 cd librdkafka-0.9.1
 ./configure && make -j $CPUS && make install
 
 # libevent-2.0.22-stable
-mkdir -p /root/source && cd /root/source
+mkdir -p /btcpool/source && cd /btcpool/source
 wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
 tar zxvf libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable
@@ -47,10 +48,10 @@ make -j $CPUS
 make install
 
 # btcpool
-mkdir -p /work && cd /work
-git clone https://github.com/btccom/btcpool.git
-cd /work/btcpool
-mkdir build && cd build
+#mkdir -p /work && cd /work
+#git clone https://github.com/btccom/btcpool.git
+cd /btcpool
+mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j $CPUS
 
